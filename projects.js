018c09a6e9ba2b590d74a4b86febf5ca536aecc7
Undex <undex.dev@gmail.com>
2025-07-25 19:54:31 +0300

    Added FishBot Site URL

--- projects.js
+++ projects.js
@@ -1,6 +1,7 @@
 const projectLinks = {
   "project-1": "https://moonux.cloud",
   "project-2": "https://moonux.cloud",
+  "project-3": "https://fishbot.eqwzzx.fun",
 };
 
 Object.entries(projectLinks).forEach(([id, url]) => {
